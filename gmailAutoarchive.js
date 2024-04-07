296739f6a02b7f07bb21aa461b950d75da907579
Toby Scholz <46481983+SpeedyGoneZales@users.noreply.github.com>
2024-04-07 11:44:17 -0400

    Update gmailAutoarchive.js
    Change message identification from search to time-based

--- gmailAutoarchive.js
+++ gmailAutoarchive.js
@@ -1,20 +1,42 @@
 function gmailAutoarchive() {
+  
+  const mapping = { // Sender, Label (aka folder)
+    "ACM Learning Center <learning@acm.org>": "[Google Mail]/ACM",  //'[Google Mail]' is specific to my configuration and not normally needed
+    "ACM TechNews <technews-editor@acm.org> ": "[Google Mail]/ACM",
+    "ACM Bulletins <acmbulletin@acm.org>": "[Google Mail]/ACM",
+  };
 
-  var delayDays = 14; 
-  var labels = ['[Google Mail]/GMX Newsletters', '[Google Mail]/Amex']
-  var maxDate = new Date();
+  const delayDays = 31; 
+  const maxDate = new Date();
   maxDate.setDate(maxDate.getDate()-delayDays); // what was the date at that time?
 
-
-  function processArchiving(labels) {
-    for (var i = 0; i < labels.length; i++) {
-      var label = GmailApp.getUserLabelByName(labels[i]);
-      var threads = label.getThreads(0, 400);
-   
-      for (var i = 0; i < threads.length; i++) {
-        if (threads[i].getLastMessageDate()<maxDate) {
-          threads[i].moveToArchive();
-      }
-    }
+  function checkAge(messageDate) {
+    return messageDate < maxDate;
   }
+  
+  function archiveMessages(threads) {
+    for (var i = 0; i < threads.length; i++) {
+      thread = threads[i];
+      messages = thread.getMessages();
+      senderEmail = messages[0].getFrom();
+      messageDate = messages[0].getDate();
+      if (checkAge(messageDate) && senderEmail in mapping) {
+        var label = GmailApp.getUserLabelByName(mapping[senderEmail]);
+        console.info("Moving message from ", senderEmail, " on ", messageDate, " to ", label.getName());
+        thread.addLabel(label); 
+        thread.moveToArchive(); // Move out of Inbox
+      } //if
+    } //for
+  } //archiveMessages
+
+  function searchInbox() {
+    for (const [key, value] of Object.entries(mapping)) {
+      var searchString = "from:" + key + " in:inbox";
+      console.info("searchString " + searchString);
+      const threads = GmailApp.search(searchString);
+      archiveMessages(threads);
+    } //for
+  } //searchInbox
+
+  searchInbox();
 }
